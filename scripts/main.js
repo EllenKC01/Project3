763cdc1c3e489deb8b10271d49b022c8ef75801d
EllenKC01 <ellen.k.cochran@gmail.com>
2021-05-04 02:18:38 -0400

    added accordions and fixed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,38 @@ function handleStateClick(e) {
 states.forEach((state) => {
     state.addEventListener("click", handleStateClick)
 });
+
+// bring user back to map from state <div>s
+// only issue here is the function only works after the user has clicked a state- otherwise the button goes nowhere
+let map = document.querySelectorAll("h3");
+
+function backToMap(e) {
+    console.log(e.target.dataset.name);
+    window.location.hash = e.target.dataset.name
+}
+
+map.forEach((back) => {
+    back.addEventListener("click", backToMap)
+});
+
+
+// create a function to open accordion panels when park name is clicked
+
+var acc = document.getElementsByClassName("park");
+var i;
+// assuming this method is necessary because we are using class names, not element names
+// for each of these elements, listen for a click
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    // if park is clicked, its panel is set to active
+      this.classList.toggle("active");
+
+    // for the active panel- if it is visible, close it; if not, open it
+      var description = this.nextElementSibling;
+      if (description.style.display === "block") {
+        description.style.display = "none";
+      } else {
+        description.style.display = "block";
+      }
+  });
+}
